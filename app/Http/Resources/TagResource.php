41412c8262b573e047f0fbf5137e8fcea9e386d9
e.andreev <k9prP46sXJ>
2023-01-08 15:43:40 +0300

    All tags

--- app/Http/Resources/TagResource.php
+++ app/Http/Resources/TagResource.php
@@ -7,31 +7,19 @@ use Illuminate\Support\Str;
 
 class TagResource extends JsonResource
 {
-
     public $preserveKeys = true;
     public static $wrap = 'tag';
 
-    /**
-     * Transform the resource collection into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
-     */
     public function toArray($request)
     {
         return [
             'id' => $this->id,
             'name' => $this->name,
             'count' => $this->COUNT,
-            'url' => $this->url,
-            'flag' => $this->flag,
-            'locations' => $this->locations,
-            'children' => $this->children,
+//            'url' => $this->url,
+//            'flag' => $this->flag,
+//            'locations' => $this->locations,
+//            'children' => $this->children,
         ];
-    }
-
-    public function with($request)
-    {
-        return ['status' => 'success'];
     }
 }
