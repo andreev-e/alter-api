a97e13dedd6d51acd767257f8b3ae7fdb138d875
e.andreev <k9prP46sXJ>
2023-01-21 11:05:07 +0300

    Poi

--- app/Models/Poi.php
+++ app/Models/Poi.php
@@ -3,20 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasManyThrough;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Poi extends Model
 {
     protected $table = 'poi';
 
-    public function tags() : HasManyThrough
+    public function tags(): BelongsToMany
     {
-        return $this->hasManyThrough(Tag::class, Relationship::class, 'POSTID', 'TAGID');
+        return $this->belongsToMany(Tag::class, 'relationship', 'TAGID')
+            ->withPivot('POSTID');
     }
+
     public function locations()
     {
-        return $this->belongsToMany(Tag::class, 'relationship', 'POSTID', 'TAGID')->where('TYPE', '<>', 0)->orderBy('COUNT', 'desc');
+        return $this->belongsToMany(Tag::class, 'relationship', 'POSTID', 'TAGID')->where('TYPE', '<>',
+            0)->orderBy('COUNT', 'desc');
     }
+
     public function twits()
     {
         return $this->hasMany(Comment::class, 'backlink')->where('approved', '=', 1)->orderBy('time', 'desc');
--- app/Models/Relationship.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Models;
-
-class Relationship
-{
-    protected $table = 'relationship';
-}
