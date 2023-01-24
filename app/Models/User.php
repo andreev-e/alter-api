e8b386b07e2f2ed7fbe3b3d74f31ed1f96b78bba
e.andreev <k9prP46sXJ>
2023-01-24 15:33:55 +0300

    User hide unnesessary fields

--- app/Models/User.php
+++ app/Models/User.php
@@ -30,6 +30,11 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
+        'userid',
+        'email_verified_at',
+        'updated_at',
+        'created_at',
+        'password_new',
     ];
 
     /**
