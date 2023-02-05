7858b56b24125632ea601e17eacc3bfce504375e
e.andreev <k9prP46sXJ>
2023-02-05 11:00:09 +0300

    Nearest

--- app/Http/Resources/PoiResource.php
+++ app/Http/Resources/PoiResource.php
@@ -11,15 +11,16 @@ class PoiResource extends PoiResourceCollection
 {
     public function toArray($request): array
     {
-        return [
-            ...parent::toArray($request),
-            'description' => strip_tags(htmlspecialchars_decode($this->description)),
-            'route' => htmlspecialchars_decode($this->route),
-            'route_o' => htmlspecialchars_decode($this->route_o),
-            'nearest' => PoiResourceCollection::collection($this->nearest),
-            'tags' => TagResource::collection($this->whenLoaded('tags')),
-            'locations' => TagResource::collection($this->whenLoaded('locations')),
-            'addon' => $this->addon,
-        ];
+        return array_merge(
+            parent::toArray($request),
+            [
+                'description' => strip_tags(htmlspecialchars_decode($this->description)),
+                'route' => htmlspecialchars_decode($this->route),
+                'route_o' => htmlspecialchars_decode($this->route_o),
+                'nearest' => PoiResourceCollection::collection($this->nearest),
+                'tags' => TagResource::collection($this->whenLoaded('tags')),
+                'locations' => TagResource::collection($this->whenLoaded('locations')),
+                'addon' => $this->addon,
+            ]);
     }
 }
