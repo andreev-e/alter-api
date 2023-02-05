d5b228905dc6da4170d9e7361150611db988a3cf
e.andreev <k9prP46sXJ>
2023-02-05 09:24:09 +0300

    Nearest

--- app/Models/Poi.php
+++ app/Models/Poi.php
@@ -29,7 +29,7 @@ class Poi extends Model
     {
         return Cache::remember('nearest:' . $this->id, 5000, static function() {
             return self::query()
-                ->select(DB::raw('(6371 * acos(cos(radians($lat)) * cos(radians(lat)) * cos(radians(lng) - radians($lng)) + sin(radians($lat)) * sin(radians(lat)))) AS `dist`'))
+                ->select(DB::raw('(6371 * acos(cos(radians(lat)) * cos(radians(lat)) * cos(radians(lng) - radians(lng)) + sin(radians(lat)) * sin(radians(lat)))) AS `dist`'))
                 ->orderBy('dist', 'desc')->limit(5)->get();
         });
     }
