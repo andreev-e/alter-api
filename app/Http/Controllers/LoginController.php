27f4861fef89f9b9c3d168fb60088d62c43bc6dc
e.andreev <k9prP46sXJ>
2023-02-06 21:14:17 +0300

    Login ok

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\LoginRequest;
-use Illuminate\Http\Request;
+use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use Symfony\Component\Console\Input\Input;
 
 class LoginController extends Controller
 {
@@ -14,8 +15,18 @@ class LoginController extends Controller
 
         if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
-
             return response()->json('Ok');
+        }
+
+        $user = User::query()->where('email', '=', $credentials['email'])->first();
+
+        if (isset($user)) {
+            if ($user->password == md5($credentials['password'])) {
+//                $user->password = Hash::make(Input::get('password'));
+//                $user->save();
+                Auth::login($user);
+                return response()->json('Ok');
+            }
         }
 
         return response()->json('Wrong credentials');
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,20 +2,13 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, Notifiable;
+    use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var string[]
-     */
     protected $fillable = [
         'name',
         'email',
@@ -45,4 +38,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function getAuthIdentifierName()
+    {
+        return 'username';
+    }
 }
