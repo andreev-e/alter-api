2ce5b359fea7f29b00778d0006cfbab38d5f4f41
e.andreev <k9prP46sXJ>
2023-02-09 15:57:59 +0300

    Poi edit

--- routes/web.php
+++ routes/web.php
@@ -20,14 +20,14 @@ Route::post('/api/login', [LoginController::class, 'authenticate'])->name('login
 Route::post('/api/logout', [LoginController::class, 'logout'])->name('logout');
 
 
-Route::prefix('/api/comment')->controller(CommentController::class)->group(function() {
+Route::prefix('/api/comment')->name('comment')->controller(CommentController::class)->group(function() {
     Route::post('', 'store')->name('store');
     Route::patch('{comment:commentid}', 'update')->name('update');
     Route::post('{comment:commentid}/approve', 'approve')->name('approve');
     Route::delete('{comment:commentid}', 'destroy')->name('destroy');
 });
 
-Route::prefix('/api/poi')->controller(PoiController::class)->group(function() {
+Route::prefix('/api/poi')->name('poi')->controller(PoiController::class)->group(function() {
     Route::post('', 'store')->name('store');
     Route::patch('{poi}', 'update')->name('update');
     Route::delete('{poi}', 'destroy')->name('destroy');
