99b2d30f2174a5c93e5e6ff63c574a166fec42e7
e.andreev <k9prP46sXJ>
2023-02-10 12:34:24 +0300

    Image upload

--- app/Http/Controllers/PoiController.php
+++ app/Http/Controllers/PoiController.php
@@ -146,6 +146,7 @@ class PoiController extends Controller
 
             if ($request->hasFile('image') && $request->file('image')->isValid()) {
                 $poi->addMediaFromRequest('image')
+                    ->storingConversionsOnDisk('s3')
                     ->toMediaCollection('image');
             }
 
