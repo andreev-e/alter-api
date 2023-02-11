e9e41247a2269fa6fb4ff0c9eb52ca8adab7a7c4
e.andreev <k9prP46sXJ>
2023-02-11 20:15:41 +0300

    artisan fix

--- app/Console/Commands/FillUrls.php
+++ app/Console/Commands/FillUrls.php
@@ -8,35 +8,15 @@ use Illuminate\Support\Str;
 
 class FillUrls extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
     protected $signature = 'fill:url';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
     protected $description = 'Fill urls';
 
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         parent::__construct();
     }
 
-    /**
-     * Execute the console command.
-     *
-     * @return int
-     */
     public function handle()
     {
         $tags = Tag::query()->where('url', '')->get();
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -29,7 +29,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command(FillUrls::class)->daily();
+        $schedule->command(FillUrls::class)->everyMinute();
         $schedule->command(RegionsProcess::class)->daily();
         $schedule->command(TagCount::class)->daily();
 
