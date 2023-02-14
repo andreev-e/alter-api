a6abc987a42abff5427f846807cbd151f1d63208
e.andreev <k9prP46sXJ>
2023-02-14 19:43:33 +0300

    CSRF

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/api/user', [LoginController::class, 'user'])->name('user.login');
+
 Route::post('login', [LoginController::class, 'authenticate'])->name('login');
 Route::post('logout', [LoginController::class, 'logout'])->name('logout');
 
--- routes/web.php
+++ routes/web.php
@@ -1,1 +1,5 @@
 <?php
+
+use App\Http\Controllers\LoginController;
+
+Route::get('/api/api/user', [LoginController::class, 'user'])->name('user.login');
