5bbd025798243f26e3cacc179f44c007441200e4
e.andreev <k9prP46sXJ>
2023-02-14 19:59:49 +0300

    CSRF

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get('/api/user', [LoginController::class, 'user'])
-//    ->middleware('auth')
+    ->middleware('auth')
     ->name('user.login');
 
 Route::post('login', [LoginController::class, 'authenticate'])->name('login');
