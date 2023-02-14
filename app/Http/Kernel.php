798238c748ae705f259028a3ad846adaa1fd9522
e.andreev <k9prP46sXJ>
2023-02-14 20:48:20 +0300

    Auth refactor

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -37,13 +37,11 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            'cors',
         ],
 
         'api' => [
 //            EnsureFrontendRequestsAreStateful::class,
             'throttle:api',
-            'cors',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Session\Middleware\StartSession::class,
@@ -66,6 +64,5 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'cors' => \App\Http\Middleware\Cors::class,
     ];
 }
