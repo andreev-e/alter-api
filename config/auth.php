a105abbd729f02c8df62b21d15e9d26a07c9f37d
e.andreev <k9prP46sXJ>
2023-02-14 19:41:36 +0300

    CSRF

--- config/auth.php
+++ config/auth.php
@@ -11,10 +11,6 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
-        'web' => [
-            'driver' => 'session',
-            'provider' => 'users',
-        ],
     ],
 
     'providers' => [
