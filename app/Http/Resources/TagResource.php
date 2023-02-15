5784b4ce39fce2e7aaca95f874ebdfea86d17451
e.andreev <k9prP46sXJ>
2023-02-15 13:33:30 +0300

    Location tags

--- app/Http/Resources/TagResource.php
+++ app/Http/Resources/TagResource.php
@@ -14,6 +14,11 @@ class TagResource extends TagResourceCollection
         return array_merge(
             collect(parent::toArray($request))->except('dist')->toArray(),
             [
+                'lat' => $this->lat,
+                'lng' => $this->lng,
+                'zoom' => $this->scale,
+                'parents' => $this->getParents(),
+                'children' => self::collection($this->whenLoaded('children')),
                 'tags' => TagResourceCollection::collection($this->tags),
             ]);
     }
