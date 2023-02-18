6a4dfc514d790421f6bfbbbe7d51d28847b2c7f8
e.andreev <k9prP46sXJ>
2023-02-18 09:51:11 +0300

    Public upload

--- app/Http/Controllers/PoiController.php
+++ app/Http/Controllers/PoiController.php
@@ -190,9 +190,9 @@ class PoiController extends Controller
                 $media->setCustomProperty('temporary_url', $localPath);
                 $media->save();
 
+                dump($localPath);
                 $img->resize($poi::THUMB_SIZE, $poi::THUMB_SIZE);
-//                Storage::disk('public')->put($folder, $img, 'public');
-                dump(asset($localPath));
+                Storage::disk('public')->put('storage/' . $folder, $img, 'public');
                 //https://api.altertravel.ru/storage/temporary-images/1676700560/5H9geOoTYSagw4ClVfEXXzPZYME8Bvkoe0WbnXOg.jpg
             }
 
