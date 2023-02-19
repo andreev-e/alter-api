3c705ff5df1e3fc253ce479aeb059a236be712d8
e.andreev <k9prP46sXJ>
2023-02-19 17:54:13 +0300

    User photo edit

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -22,6 +22,7 @@ class UserResource extends JsonResource
             'lat' => $this->lat,
             'lng' => $this->lng,
             'thumb' => $this->getFirstMediaUrl('user-image', 'thumb'),
+            'images' => ImageResource::collection($this->media),
         ];
     }
 }
