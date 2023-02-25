3e8fa6353ac6a6f14400c1e236f4563724fd95e2
e.andreev <k9prP46sXJ>
2023-02-25 15:11:54 +0300

    Favorites

--- app/Models/User.php
+++ app/Models/User.php
@@ -49,6 +49,7 @@ class User extends Authenticatable  implements HasMedia
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'favorites' => 'array',
     ];
 
     public function getAuthIdentifierName()
