0a529e0b085939ccbc884b1e310401e94336955d
e.andreev <k9prP46sXJ>
2023-02-25 08:52:20 +0300

    Geocode scheduled fix

--- app/Console/Commands/FillLocations.php
+++ app/Console/Commands/FillLocations.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use App\Jobs\PoiGeocodeJob;
 use App\Models\Poi;
 use Illuminate\Console\Command;
+use Illuminate\Database\Eloquent\Builder;
 
 class FillLocations extends Command
 {
@@ -19,7 +20,13 @@ class FillLocations extends Command
 
     public function handle()
     {
-        $pois = Poi::query()->oldest()->limit(950)->get();
+        $pois = Poi::query()
+            ->where('show', 1)
+            ->whereNotExists(function($query) {
+                $query->select()->from('location_poi')
+                    ->whereRaw('`poi`.`id` = `location_poi`.`poi_id`');
+            })
+            ->limit(950)->get();
         foreach ($pois as $poi) {
             PoiGeocodeJob::dispatch($poi);
         }
