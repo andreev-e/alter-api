8c50d4ec81e4c93d302217167c2eb4911f436b12
e.andreev <k9prP46sXJ>
2023-03-04 14:22:47 +0300

    Telegram test

--- app/Telegram/Commands/GetnearestpoisCommand.php
+++ app/Telegram/Commands/GetnearestpoisCommand.php
@@ -4,16 +4,25 @@
 namespace App\Telegram\Commands;
 
 
+use Longman\TelegramBot\Commands\UserCommand;
 use Longman\TelegramBot\Entities\ServerResponse;
-use PhpTelegramBot\Laravel\Telegram\Commands\GenericmessageCommand;
 
-class GetnearestpoisCommand extends GenericmessageCommand
+class GetnearestpoisCommand extends UserCommand
 {
-    /**
-     * @throws \Longman\TelegramBot\Exception\TelegramException
-     */
     public function execute(): ServerResponse
     {
-        return $this->replyToChat($this->getMessage()->getLocation());
+        $message = $this->getMessage();
+        $txt = trim($message->getText(true));
+        $chat_id = $message->getChat()->getId();
+        $user_id = $message->getFrom()->getId();
+        if($txt === "genericmessage") {
+            $data = [];
+            $data['chat_id'] = $chat_id;
+            $data['reply_to_message_id'] = $message->message_id;
+            $data['text'] = "a reply to a generic message";
+            Request::sendMessage($data);
+        }
+
+        return Request::emptyResponse();
     }
 }
