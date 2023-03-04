a95c7925870076d4bd3d0c684976b7dd977795df
e.andreev <k9prP46sXJ>
2023-03-04 15:29:14 +0300

    Telegram test

---
+++ app/Telegram/Commands/MessageCommand.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Telegram\Commands;
+
+use Longman\TelegramBot\Commands\SystemCommands\GenericmessageCommand;
+use Longman\TelegramBot\Entities\ServerResponse;
+
+class MessageCommand extends GenericmessageCommand
+{
+    public function execute(): ServerResponse
+    {
+        $message = $this->getMessage();
+
+        $message_text = $message->getText(true);
+
+        $this->replyToChat($message_text);
+    }
+}
