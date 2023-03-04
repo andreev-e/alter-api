0fc5c9d4590b1feca015a6f453532480f62c2c36
e.andreev <k9prP46sXJ>
2023-03-04 15:01:22 +0300

    Telegram test

---
+++ app/Telegram/Commands/NearestCommand.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace App\Telegram\Commands;
+
+
+use Longman\TelegramBot\Commands\UserCommand;
+use Longman\TelegramBot\Entities\ServerResponse;
+use Longman\TelegramBot\Exception\TelegramException;
+
+class NearestCommand extends UserCommand
+{
+
+    /** @var string Command name */
+    protected $name = 'nearest';
+    /** @var string Command description */
+    protected $description = '';
+    /** @var string Usage description */
+    protected $usage = '/nearest';
+    /** @var string Version */
+    protected $version = '1.0.0';
+
+    public function execute(): ServerResponse
+    {
+
+    }
+
+}
