03afc892bae9431fa71ea484c2cccdd8493daefd
e.andreev <k9prP46sXJ>
2023-03-07 10:09:21 +0300

    Poi date of create & update

--- app/Http/Resources/PoiResource.php
+++ app/Http/Resources/PoiResource.php
@@ -26,6 +26,8 @@ class PoiResource extends PoiResourceCollection
                 'copyright' => $this->copyright,
                 'dominatecolor' => $this->dominatecolor,
                 'images' => ImageResource::collection($this->media),
+                'created_at' => $this->created_at,
+                'updated_at' => $this->updated_at,
             ]);
     }
 }
