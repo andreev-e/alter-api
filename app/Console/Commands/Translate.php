faa76a310c50fb586f0a1c65e24177ad9a9a5533
e.andreev <k9prP46sXJ>
2023-03-29 19:00:16 +0300

    Translations

--- app/Console/Commands/Translate.php
+++ app/Console/Commands/Translate.php
@@ -21,11 +21,12 @@ class Translate extends Command
     {
         $pois = Poi::query()->whereNull('name_en')
             ->orderBy('views', 'desc')
-            ->limit(1)->get();
+            ->limit(1000)->get();
         foreach ($pois as $poi) {
             $poi->name_en = $translator->translate($poi->name, 'ru', 'en', 'Необходимо перевести заголовок для публикации https://altertravel.ru/poi/' . $poi->id);
             $poi->timestamps = false;
             $poi->save();
+            echo $poi->id . ' ' . $poi->name . ' ' . $poi->name_en . PHP_EOL;
         }
     }
 }
