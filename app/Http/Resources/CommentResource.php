8cc8ca861b88b5385bb2c852b31f626e35c505cb
e.andreev <k9prP46sXJ>
2023-04-16 07:22:29 +0300

    Name en comments

--- app/Http/Resources/CommentResource.php
+++ app/Http/Resources/CommentResource.php
@@ -14,6 +14,7 @@ class CommentResource extends JsonResource
             'commentid' => $this->commentid,
             'name' => $this->name,
             'object_name' => $this->commentable->name,
+            'object_name_en' => $this->commentable->name_en,
             'object_id' => $this->commentable->id,
             'object_type' => $this->commentable::TYPE,
             'user' => new UserResourceCollection($this->whenLoaded('user')),
